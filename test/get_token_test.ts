d716bf1215fac769b9ee407fc853feca47d5c8b4
zuisong <com.me@foxmail.com>
2024-03-11 14:22:12 +0800

    🌱 chore: fmt code

--- test/get_token_test.ts
+++ test/get_token_test.ts
@@ -1,26 +1,13 @@
-import { expect } from "jsr:@std/expect"
+import { assertEquals } from "jsr:@std/assert"
 import { getToken } from "../src/utils.ts"
-import type { ApiParam } from "../src/utils.ts"
 
 Deno.test("get token_test", () => {
-  expect(getToken({ authorization: "Bearer my_key" })).toStrictEqual({
-    apikey: "my_key",
-    useBeta: false,
-  } satisfies ApiParam)
-  expect(getToken({ other_key: "Bearer my_key" })).toStrictEqual(null)
-  expect(getToken({ other_key: "my_key" })).toStrictEqual(null)
-  expect(getToken({ authorization: "my_key" })?.apikey).toStrictEqual("my_key")
-  expect(getToken({ Authorization: "my_key" })?.apikey).toStrictEqual("my_key")
-  expect(getToken({ Authorization: "Bearer my_key#useBeta" })).toStrictEqual({
-    apikey: "my_key",
-    useBeta: true,
-  } satisfies ApiParam)
-  expect(getToken({ Authorization: "bearer my_key#useBeta" })).toStrictEqual({
-    apikey: "my_key",
-    useBeta: true,
-  } satisfies ApiParam)
-  expect(getToken({ Authorization: "bearer my_key#otherConfig" })).toStrictEqual({
-    apikey: "my_key",
-    useBeta: false,
-  } satisfies ApiParam)
+  assertEquals(getToken({ authorization: "Bearer my_key" }), { apikey: "my_key", useBeta: false })
+  assertEquals(getToken({ other_key: "Bearer my_key" }), null)
+  assertEquals(getToken({ other_key: "my_key" }), null)
+  assertEquals(getToken({ authorization: "my_key" }), { apikey: "my_key", useBeta: false })
+  assertEquals(getToken({ Authorization: "my_key" }), { apikey: "my_key", useBeta: false })
+  assertEquals(getToken({ Authorization: "Bearer my_key#useBeta" }), { apikey: "my_key", useBeta: true })
+  assertEquals(getToken({ Authorization: "bearer my_key#useBeta" }), { apikey: "my_key", useBeta: true })
+  assertEquals(getToken({ Authorization: "bearer my_key#otherConfig" }), { apikey: "my_key", useBeta: false })
 })
