8060ddda8599f1fe2b1997d697e239b15b77e175
zuisong <com.me@foxmail.com>
2024-05-09 13:46:38 +0800

    🌱 chore: function call improvement

--- src/openai/chat/completions/StreamingChatProxyHandler.ts
+++ src/openai/chat/completions/StreamingChatProxyHandler.ts
@@ -1,14 +1,31 @@
 import { generateContent } from "../../../gemini-api-client/gemini-api-client.ts"
+import type { FunctionCall } from "../../../gemini-api-client/types.ts"
 import type { OpenAI } from "../../../types.ts"
 import { type ApiParam, genModel } from "../../../utils.ts"
 
 export async function* streamingChatProxyHandler(req: OpenAI.Chat.ChatCompletionCreateParams, apiParam: ApiParam) {
   const [model, geminiReq] = genModel(req)
-  const geminiResp: string = await generateContent(apiParam, model, geminiReq)
+  const geminiResp: string | FunctionCall = await generateContent(apiParam, model, geminiReq)
     .then((it) => it.response.result())
-    .catch((e) => e.message ?? e?.toString())
+    .catch((e: Error) => e.message ?? e?.toString())
 
-  function genOpenAiResp(content: string, stop: boolean) {
+  function genOpenAiResp(content: string | FunctionCall, stop: boolean) {
+    if (typeof content === "string") {
+      return {
+        id: "chatcmpl-abc123",
+        object: "chat.completion.chunk",
+        created: Math.floor(Date.now() / 1000),
+        model: req.model,
+        choices: [
+          {
+            delta: { role: "assistant", content: content },
+            finish_reason: stop ? "stop" : null,
+            index: 0,
+          },
+        ],
+      } satisfies OpenAI.Chat.ChatCompletionChunk
+    }
+
     return {
       id: "chatcmpl-abc123",
       object: "chat.completion.chunk",
@@ -16,8 +33,8 @@ export async function* streamingChatProxyHandler(req: OpenAI.Chat.ChatCompletion
       model: req.model,
       choices: [
         {
-          delta: { role: "assistant", content: content },
-          finish_reason: stop ? "stop" : null,
+          delta: { role: "assistant", function_call: content },
+          finish_reason: stop ? "function_call" : null,
           index: 0,
         },
       ],
