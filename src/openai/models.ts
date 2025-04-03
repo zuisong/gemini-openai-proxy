49dcb1089aebfe31e8870e469c5556f5d467d555
zuisong <com.me@foxmail.com>
2025-04-03 18:05:10 +0800

    models

--- src/openai/models.ts
+++ src/openai/models.ts
@@ -1,19 +1,25 @@
-import type { OpenAI } from "../types.ts"
-import { ModelMapping } from "../utils.ts"
-export const modelData: OpenAI.Models.Model[] = Object.keys(ModelMapping).map((model) => ({
+import { models as listModels } from "../gemini-api-client/gemini-api-client.ts";
+import type { OpenAI } from "../types.ts";
+import { getToken, ModelMapping } from "../utils.ts";
+export const modelData: OpenAI.Models.Model[] = Object.keys(ModelMapping).map((
+  model,
+) => ({
   created: 1677610602,
   object: "model",
   owned_by: "openai",
   id: model,
-}))
+}));
 
-export const models = () => {
-  return {
-    object: "list",
-    data: modelData,
-  }
-}
+export const models = async (req: Request) => {
+  const apiParam =  getToken(req.headers);
+  return await listModels(apiParam);
+
+  // return {
+  //   object: "list",
+  //   data: modelData,
+  // }
+};
 
 export const modelDetail = (model: string) => {
-  return modelData.find((it) => it.id === model)
-}
+  return modelData.find((it) => it.id === model);
+};
